bd39c5d9a1d5aff4e36057808c6e54a2ee35edab
noramirulnordin <noramirul.nordin@pipeline-network.com>
2022-10-18 16:31:33 +0800

    fix migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            \Illuminate\Support\Facades\DB::statement('SET SESSION sql_require_primary_key=0');
             $table->id();
             $table->string('nama');
             $table->string('no_kakitangan');
