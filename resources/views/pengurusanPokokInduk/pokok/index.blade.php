cfadeb2919afeef06ffa74d2c319da45607a0a3b
noramirulnordin <noramirul.nordin@pipeline-network.com>
2022-10-27 07:29:39 +0800

    design

--- resources/views/pengurusanPokokInduk/pokok/index.blade.php
+++ resources/views/pengurusanPokokInduk/pokok/index.blade.php
@@ -20,66 +20,6 @@
                 </div>
             </div>
 
-            <div class="row mb-3">
-                <div class="col-xl-6">
-                    <div class="row g-3 align-items-center">
-                        <div class="col-xl-2">
-                            <label class="col-form-label">Blok</label>
-                        </div>
-                        <div class="col-xl-8">
-                            <input type="text"class="form-control border-danger" placeholder="SILA TAIP DISINI">
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-xl-6">
-                    <div class="row g-3 align-items-center">
-                        <div class="col-xl-4 text-center">
-                            <label class="col-form-label">Progeny</label>
-                        </div>
-                        <div class="col-xl-8">
-                            <input type="text"class="form-control border-danger" placeholder="SILA TAIP DISINI">
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-xl-6">
-                    <div class="row g-3 align-items-center">
-                        <div class="col-xl-2">
-                            <label class="col-form-label">Baka</label>
-                        </div>
-                        <div class="col-xl-8">
-                            <input type="text"class="form-control border-danger" placeholder="SILA TAIP DISINI">
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-xl-6">
-                    <div class="row g-3 align-items-center">
-                        <div class="col-xl-4 text-center">
-                            <label class="col-form-label">No. Pokok</label>
-                        </div>
-                        <div class="col-xl-8">
-                            <input type="text"class="form-control border-danger" placeholder="SILA TAIP DISINI">
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row mt-5">
-                <div class="text-center">
-                    <button class="btn btn-sm btn-danger">Cari
-                        <span data-feather="search"></span>
-                    </button>
-                    <button class="btn btn-sm btn-link">
-                        <span class="refreshbtn" style="color:grey" data-feather="refresh-ccw"></span>
-                    </button>
-                </div>
-            </div>
-
-
             <div class="text-end mb-3 mt-5">
                 <a href="{{ route('pi.p.create') }}" class="btn btn-danger">Daftar
                     <span class="text-white" data-feather="plus-circle"></span>
@@ -88,68 +28,57 @@
             <div class="row">
                 <div class="card">
                     <div class="card-body">
-                        <div id="tableExample2"
-                            data-list='{"valueNames":["bil","blok","baka","progeny","noPokok"],"page":5,"pagination":true}'>
-                            <div class="table-responsive scrollbar table-striped">
-                                <table class="table fs--1 mb-0 text-center">
-                                    <thead class=" text-900">
-                                        <tr style="border-bottom-color: #F89521">
-                                            <th class="sort" data-sort="bil">Bil</th>
-                                            <th class="sort" data-sort="blok">Blok</th>
-                                            <th class="sort" data-sort="baka">Baka</th>
-                                            <th class="sort" data-sort="progeny">Progeny</th>
-                                            <th class="sort" data-sort="noPokok">No Pokok</th>
-                                            <th>Tindakan</th>
+                        <div class="table-responsive scrollbar table-striped">
+                            <table class="table fs--1 mb-0 text-center datatable">
+                                <thead class=" text-900">
+                                    <tr style="border-bottom-color: #F89521">
+                                        <th>Bil</th>
+                                        <th>Blok</th>
+                                        <th>Baka</th>
+                                        <th>Progeny</th>
+                                        <th>No Pokok</th>
+                                        <th>Tindakan</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="list">
+                                    @foreach ($pokoks as $pokok)
+                                        <tr style="border-bottom:#fff">
+                                            <td>
+                                                {{ $loop->iteration }}
+                                            </td>
+                                            <td>
+                                                {{ $pokok->no_pokok }}
+                                            </td>
+                                            <td>
+                                                {{ $pokok->blok }}
+                                            </td>
+                                            <td>
+                                                {{ $pokok->baka }}
+                                            </td>
+                                            <td>
+                                                {{ $pokok->progeny }}
+                                            </td>
+                                            <td>
+                                                <form action="{{ route('pi.p.delete', $pokok->id) }}" method="post"
+                                                    class="d-inline-flex">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <button class=" btn btn-sm btn-danger">
+                                                        <span data-feather="trash-2" style="width:15px;"></span>
+                                                    </button>
+                                                </form>
+
+                                                <a href="{{ route('pi.p.edit', $pokok->id) }}"
+                                                    class="ms-2 btn btn-sm btn-danger">
+                                                    <span data-feather="edit" style="width:15px;"></span>
+                                                </a>
+                                            </td>
                                         </tr>
-                                    </thead>
-                                    <tbody class="list">
-                                        @foreach ($pokoks as $pokok)
-                                            <tr style="border-bottom:#fff">
-                                                <td class="bil">
-                                                    {{ $loop->iteration }}
-                                                </td>
-                                                <td class="blok">
-                                                    {{ $pokok->blok }}
-                                                </td>
-                                                <td class="baka">
-                                                    {{ $pokok->baka }}
-                                                </td>
-                                                <td class="progeny">
-                                                    {{ $pokok->progeny }}
-                                                </td>
-                                                <td class="noPokok">
-                                                    {{ $pokok->no_pokok }}
-                                                </td>
-                                                <td>
-                                                    <form action="{{ route('pi.p.delete', $pokok->id) }}" method="post"
-                                                        class="d-inline-flex">
-                                                        @csrf
-                                                        @method('delete')
-                                                        <button class=" btn btn-sm btn-danger">
-                                                            <span data-feather="trash-2" style="width:15px;"></span>
-                                                        </button>
-                                                    </form>
+                                    @endforeach
 
-                                                    <a href="{{ route('pi.p.edit', $pokok->id) }}"
-                                                        class="ms-2 btn btn-sm btn-danger">
-                                                        <span data-feather="edit" style="width:15px;"></span>
-                                                    </a>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-
-                                    </tbody>
-                                </table>
-                            </div>
-                            <div class="d-flex justify-content-center mt-3">
-                                <button class="btn btn-sm btn-falcon-default me-1" type="button" title="Previous"
-                                    data-list-pagination="prev"><span class="fas fa-chevron-left"></span></button>
-                                <ul class="pagination mb-0"></ul>
-                                <button class="btn btn-sm btn-falcon-default ms-1" type="button" title="Next"
-                                    data-list-pagination="next"><span class="fas fa-chevron-right"> </span></button>
-                            </div>
+                                </tbody>
+                            </table>
                         </div>
-
                     </div>
                 </div>
             </div>
--- resources/views/pengurusanPokokInduk/tandan/index.blade.php
+++ resources/views/pengurusanPokokInduk/tandan/index.blade.php
@@ -3,7 +3,7 @@
     <x-header main="Pengurusan Pokok Induk" sub="Tandan" sub2="" />
 
 
-    <div class="row justify-content-center mt-5">
+    {{-- <div class="row justify-content-center mt-5">
         <div class="col-8">
             <div class="row align-items-center">
                 <div class="col-2 text-end">
@@ -33,89 +33,77 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 
 
 
-    <div class="row justify-content-center mt-7">
+    <div class="row justify-content-center mt-5">
         <div class="col-10">
             <div class="d-flex flex-row-reverse mb-3">
                 <a href="{{ route('pi.t.create') }}" class="ms-3 btn btn-danger">Daftar
                     <span class="text-white" data-feather="plus-circle"></span>
                 </a>
-                <a href="{{ route('pi.t.create') }}" class="ms-3 btn btn-danger">Kod QR Pelupusan
+                {{-- <a href="{{ route('pi.t.create') }}" class="ms-3 btn btn-danger">Kod QR Pelupusan
                     <span class="text-white fas fas fa-qrcode"></span>
                 </a>
                 <a href="{{ route('pi.t.muat') }}" class="ms-3 btn btn-danger">Muat Naik Dokumen
                     <span class="text-white" data-feather="upload-cloud"></span>
-                </a>
-
+                </a> --}}
             </div>
 
             <div class="row">
                 <div class="card">
                     <div class="card-body">
-                        <div id="tableExample2"
-                            data-list='{"valueNames":["bil","no_daftar","no_pokok"],"page":5,"pagination":true}'>
-                            <div class="table-responsive scrollbar table-striped">
-                                <table class="table fs--1 mb-0 text-center">
-                                    <thead class=" text-900">
-                                        <tr style="border-bottom-color: #F89521">
-                                            <th class="sort" data-sort="bil">Bil</th>
-                                            <th class="sort" data-sort="no_daftar">No. Daftar</th>
-                                            <th class="sort" data-sort="no_pokok">No. Pokok</th>
-                                            <th>Kod QR</th>
-                                            <th>Tindakan</th>
+                        <div class="table-responsive scrollbar table-striped">
+                            <table class="table fs--1 mb-0 text-center datatable">
+                                <thead class=" text-900">
+                                    <tr style="border-bottom-color: #F89521">
+                                        <th>Bil</th>
+                                        <th>No. Daftar</th>
+                                        <th>No. Pokok</th>
+                                        <th>Kod QR</th>
+                                        <th>Tindakan</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($tandans as $tandan)
+                                        <tr style="border-bottom:#fff">
+                                            <td>
+                                                {{ $loop->iteration }}
+                                            </td>
+                                            <td>
+                                                {{ $tandan->no_daftar }}
+                                            </td>
+                                            <td>
+                                                {{ $tandan->pokok->no_pokok ?? 'Belum didaftar pokok' }}
+                                            </td>
+                                            <td>
+                                                <a href="" class="btn btn-danger btn-sm">
+                                                    <span class="fas fa-eye" style="width:15px;"></span>
+                                                </a>
+                                                <a href="" class="ms-2 btn btn-danger btn-sm">
+                                                    <span class="fas fa-download" style="width:15px;"></span>
+                                                </a>
+                                            </td>
+                                            <td>
+                                                <a href="{{ route('pi.t.edit', $tandan->id) }}"
+                                                    class="btn btn-sm btn-danger">
+                                                    <span class="fas fa-edit" style="width:15px;"></span>
+                                                </a>
+                                                <form action="{{ route('pi.t.delete', $tandan->id) }}" method="post"
+                                                    class="d-inline ms-2">
+                                                    @method('delete')
+                                                    @csrf
+                                                    <button class=" btn btn-sm btn-danger">
+                                                        <span class="fas fa-trash" style="width:15px;"></span>
+                                                    </button>
+                                                </form>
+                                            </td>
                                         </tr>
-                                    </thead>
-                                    <tbody class="list">
-                                        @foreach ($tandans as $tandan)
-                                            <tr style="border-bottom:#fff">
-                                                <td class="bil">
-                                                    {{ $loop->iteration }}
-                                                </td>
-                                                <td class="no_daftar">
-                                                    {{ $tandan->no_daftar }}
-                                                </td>
-                                                <td class="no_pokok">
-                                                    {{ $tandan->pokok->no_pokok ?? 'Belum didaftar pokok' }}
-                                                </td>
-                                                <td>
-                                                    <a href="" class="btn btn-danger btn-sm">
-                                                        <span class="fas fa-eye" style="width:15px;"></span>
-                                                    </a>
-                                                    <a href="" class="ms-2 btn btn-danger btn-sm">
-                                                        <span class="fas fa-download" style="width:15px;"></span>
-                                                    </a>
-                                                </td>
-                                                <td>
-                                                    <a href="{{ route('pi.t.edit', $tandan->id) }}"
-                                                        class="btn btn-sm btn-danger">
-                                                        <span class="fas fa-edit" style="width:15px;"></span>
-                                                    </a>
-                                                    <form action="{{ route('pi.t.delete', $tandan->id) }}" method="post"
-                                                        class="d-inline ms-2">
-                                                        @method('delete')
-                                                        @csrf
-                                                        <button class=" btn btn-sm btn-danger">
-                                                            <span class="fas fa-trash" style="width:15px;"></span>
-                                                        </button>
-                                                    </form>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                            <div class="d-flex justify-content-center mt-3">
-                                <button class="btn btn-sm btn-falcon-default me-1" type="button" title="Previous"
-                                    data-list-pagination="prev"><span class="fas fa-chevron-left"></span></button>
-                                <ul class="pagination mb-0"></ul>
-                                <button class="btn btn-sm btn-falcon-default ms-1" type="button" title="Next"
-                                    data-list-pagination="next"><span class="fas fa-chevron-right"> </span></button>
-                            </div>
+                                    @endforeach
+                                </tbody>
+                            </table>
                         </div>
-
                     </div>
                 </div>
             </div>
