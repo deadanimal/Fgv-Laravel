2ae59cec25673cac9f058594aa2ae4ae61b818d5
noramirulnordin <noramirul.nordin@pipeline-network.com>
2022-10-27 09:52:32 +0800

    fix table tandan

--- resources/views/pengurusanPokokInduk/tandan/index.blade.php
+++ resources/views/pengurusanPokokInduk/tandan/index.blade.php
@@ -78,43 +78,49 @@
                                                 {{ $tandan->pokok->no_pokok ?? 'Belum didaftar pokok' }}
                                             </td>
                                             <td>
-                                                <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal"
-                                                    data-bs-target="#modal{{ $tandan->id }}">
-                                                    <span class="fas fa-eye" style="width:15px;"></span>
-                                                </button>
-                                                <div class="modal fade" id="modal{{ $tandan->id }}" tabindex="-1"
-                                                    role="dialog" aria-hidden="true">
-                                                    <div class="modal-dialog modal-dialog-centered" role="document"
-                                                        style="max-width: 500px">
-                                                        <div class="modal-content position-relative">
-                                                            <div class="position-absolute top-0 end-0 mt-2 me-2 z-index-1">
-                                                                <button
-                                                                    class="btn-close btn btn-sm btn-circle d-flex flex-center transition-base"
-                                                                    data-bs-dismiss="modal" aria-label="Close"></button>
-                                                            </div>
-                                                            <div class="modal-body p-0">
-                                                                <div class="rounded-top-lg py-3 ps-4 pe-6 bg-light">
-                                                                    <h4 class="mb-1">QR CODE</h4>
+                                                @if ($tandan->pokok)
+                                                    <button type="button" class="btn btn-danger btn-sm"
+                                                        data-bs-toggle="modal" data-bs-target="#modal{{ $tandan->id }}">
+                                                        <span class="fas fa-eye" style="width:15px;"></span>
+                                                    </button>
+                                                    <div class="modal fade" id="modal{{ $tandan->id }}" tabindex="-1"
+                                                        role="dialog" aria-hidden="true">
+                                                        <div class="modal-dialog modal-dialog-centered" role="document"
+                                                            style="max-width: 500px">
+                                                            <div class="modal-content position-relative">
+                                                                <div
+                                                                    class="position-absolute top-0 end-0 mt-2 me-2 z-index-1">
+                                                                    <button
+                                                                        class="btn-close btn btn-sm btn-circle d-flex flex-center transition-base"
+                                                                        data-bs-dismiss="modal" aria-label="Close"></button>
                                                                 </div>
-                                                                <div class="p-4">
-                                                                    <div class="visible-print text-center">
-                                                                        {!! QrCode::size(100)->generate(URL::to('/pengurusan-pokok-induk/tandan/edit/' . $tandan->id)) !!}
-                                                                        {{-- <p>Scan me to return to the original page.</p> --}}
+                                                                <div class="modal-body p-0">
+                                                                    <div class="rounded-top-lg py-3 ps-4 pe-6 bg-light">
+                                                                        <h4 class="mb-1">QR CODE</h4>
+                                                                    </div>
+                                                                    <div class="p-4">
+                                                                        <div class="visible-print text-center">
+                                                                            {!! QrCode::size(100)->generate(URL::to('/pengurusan-pokok-induk/tandan/edit/' . $tandan->id)) !!}
+                                                                            {{-- <p>Scan me to return to the original page.</p> --}}
+                                                                        </div>
                                                                     </div>
                                                                 </div>
-                                                            </div>
-                                                            <div class="modal-footer">
-                                                                <button class="btn btn-secondary" type="button"
-                                                                    data-bs-dismiss="modal">Padam</button>
+                                                                <div class="modal-footer">
+                                                                    <button class="btn btn-secondary" type="button"
+                                                                        data-bs-dismiss="modal">Padam</button>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
-                                                </div>
+                                                    <a href="{{ route('downloadqr', $tandan->id) }}"
+                                                        class="ms-2 btn btn-danger btn-sm">
+                                                        <span class="fas fa-download" style="width:15px;"></span>
+                                                    </a>
+                                                @else
+                                                    Perlu daftar Pokok
+                                                @endif
 
-                                                <a href="{{ route('downloadqr', $tandan->id) }}"
-                                                    class="ms-2 btn btn-danger btn-sm">
-                                                    <span class="fas fa-download" style="width:15px;"></span>
-                                                </a>
+
 
                                             </td>
                                             <td>
