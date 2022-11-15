5f7b422881ab9109ad68f95adc7236066c7d7557
noramirulnordin <noramirul.nordin@pipeline-network.com>
2022-11-16 00:39:08 +0800

    text

--- resources/views/pengurusanPokokInduk/pokok/index.blade.php
+++ resources/views/pengurusanPokokInduk/pokok/index.blade.php
@@ -14,11 +14,11 @@
 
             <div class="row justify-content-center mb-5">
                 <div class="col-xl-5 border-end">
-                    <h4 class="text-center">JUMLAH AKTIF</h4>
+                    <h4 class="text-center">POKOK AKTIF</h4>
                     <h1 class="text-center text-success fw-bold">{{ $aktif }}</h1>
                 </div>
                 <div class="col-xl-6">
-                    <h4 class="text-center">TANDAN TIDAK AKTIF</h4>
+                    <h4 class="text-center">POKOK TIDAK AKTIF</h4>
                     <h1 class="text-center text-danger fw-bold">{{ $tidak_aktif }}</h1>
                 </div>
             </div>
