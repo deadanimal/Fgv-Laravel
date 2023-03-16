73f5af0ac51c348628f1481a9bf7390dd9babe5c
Afeez <afeezaziz@pipeline.com.my>
2023-03-16 15:39:23 +0000

    api

--- app/Http/Controllers/HarvestApiController.php
+++ app/Http/Controllers/HarvestApiController.php
@@ -107,7 +107,7 @@ class HarvestApiController extends Controller
         return User::withCount('harvest')->get();
     }
 
-    public function FunctionName(Request $request)
+    public function multipleHarvest(Request $request)
     {
         if (!$request->user_id) {
             return [
@@ -115,82 +115,84 @@ class HarvestApiController extends Controller
                 'message' => "Sila Masukkan User ID (user_id)",
             ];
         }
-        foreach ($request->pokok_id as $key => $pokok_id) {
-            try {
-                $request->id[$key];
-                $adaId = true;
-            } catch (\Throwable$th) {
-                $adaId = false;
-            }
 
-            if ($adaId) {
-                $harvest[$key] = Harvest::find($request->id[$key]);
-                if (!$harvest[$key]) {
-                    return [
-                        'code' => 404,
-                        'message' => "Id Harvest " . $harvest[$key] . " not found",
-                    ];
+        if ($request->pokok_id) {
+            foreach ($request->pokok_id as $key => $pokok_id) {
+                try {
+                    $request->id[$key];
+                    $adaId = true;
+                } catch (\Throwable$th) {
+                    $adaId = false;
                 }
-                $harvest[$key]->update([
-                    "pokok_id" => $pokok_id,
-                    "tandan_id" => $request->tandan_id[$key] ?? $harvest[$key]->tandan_id,
-                    "no_harvest" => $request->no_harvest[$key] ?? $harvest[$key]->no_harvest,
-                    "kerosakan_id" => $request->kerosakan_id[$key] ?? $harvest[$key]->kerosakan_id,
-                    "berat_tandan" => $request->berat_tandan[$key] ?? $harvest[$key]->berat_tandan,
-                    "catatan" => $request->catatan[$key] ?? $harvest[$key]->catatan,
-                    "pengesah_id" => $request->pengesah_id[$key] ?? $harvest[$key]->pengesah_id,
-                    "catatan_pengesah" => $request->catatan_pengesah[$key] ?? $harvest[$key]->catatan_pengesah,
-                    "jumlah_tandan" => $request->jumlah_tandan[$key] ?? $harvest[$key]->jumlah_tandan,
-                    "status" => $request->status[$key] ?? $harvest[$key]->status,
-                    "number_of_postponed_field" => $request->number_of_postponed_field[$key] ?? $harvest[$key]->number_of_postponed_field,
-                    "tambah_hari" => $request->tambah_hari[$key] ?? $harvest[$key]->tambah_hari,
-                    "status_bunga" => $request->status_bunga[$key] ?? $harvest[$key]->status_bunga,
-                    "status_bunga" => $request->status_bunga[$key] ?? $harvest[$key]->status_bunga,
-
-                ]);
-            } else {
-                $harvest[$key] = Harvest::create([
-                    "pokok_id" => $pokok_id,
-                    "tandan_id" => $request->tandan_id[$key] ?? null,
-                    "no_harvest" => $request->no_harvest[$key] ?? null,
-                    "kerosakan_id" => $request->kerosakan_id[$key] ?? null,
-                    "berat_tandan" => $request->berat_tandan[$key] ?? null,
-                    "catatan" => $request->catatan[$key] ?? null,
-                    "pengesah_id" => $request->pengesah_id[$key] ?? null,
-                    "catatan_pengesah" => $request->catatan_pengesah[$key] ?? null,
-                    "jumlah_tandan" => $request->jumlah_tandan[$key] ?? null,
-                    "status" => $request->status[$key] ?? null,
-                    "number_of_postponed_field" => $request->number_of_postponed_field[$key] ?? null,
-                    "tambah_hari" => $request->tambah_hari[$key] ?? null,
-                    "status_bunga" => $request->status_bunga[$key] ?? null,
-                    "status_bunga" => $request->status_bunga[$key] ?? null,
-                ]);
-            }
-
-        }
-
-        if ($request->hasFile('url_gambar')) {
-            foreach ($request->file('url_gambar') as $key => $value) {
-
-                $image_path = $harvest[$key]->url_gambar;
-                if (Storage::exists("public/" . $image_path)) {
-                    Storage::delete("public/" . $image_path);
+    
+                if ($adaId) {
+                    $harvest[$key] = Harvest::find($request->id[$key]);
+                    if (!$harvest[$key]) {
+                        return [
+                            'code' => 404,
+                            'message' => "Id Harvest " . $harvest[$key] . " not found",
+                        ];
+                    }
+                    $harvest[$key]->update([
+                        "pokok_id" => $pokok_id,
+                        "tandan_id" => $request->tandan_id[$key] ?? $harvest[$key]->tandan_id,
+                        "no_harvest" => $request->no_harvest[$key] ?? $harvest[$key]->no_harvest,
+                        "kerosakan_id" => $request->kerosakan_id[$key] ?? $harvest[$key]->kerosakan_id,
+                        "berat_tandan" => $request->berat_tandan[$key] ?? $harvest[$key]->berat_tandan,
+                        "catatan" => $request->catatan[$key] ?? $harvest[$key]->catatan,
+                        "pengesah_id" => $request->pengesah_id[$key] ?? $harvest[$key]->pengesah_id,
+                        "catatan_pengesah" => $request->catatan_pengesah[$key] ?? $harvest[$key]->catatan_pengesah,
+                        "jumlah_tandan" => $request->jumlah_tandan[$key] ?? $harvest[$key]->jumlah_tandan,
+                        "status" => $request->status[$key] ?? $harvest[$key]->status,
+                        "number_of_postponed_field" => $request->number_of_postponed_field[$key] ?? $harvest[$key]->number_of_postponed_field,
+                        "tambah_hari" => $request->tambah_hari[$key] ?? $harvest[$key]->tambah_hari,
+                        "status_bunga" => $request->status_bunga[$key] ?? $harvest[$key]->status_bunga,
+                        "status_bunga" => $request->status_bunga[$key] ?? $harvest[$key]->status_bunga,
+    
+                    ]);
+                } else {
+                    $harvest[$key] = Harvest::create([
+                        "pokok_id" => $pokok_id,
+                        "tandan_id" => $request->tandan_id[$key] ?? null,
+                        "no_harvest" => $request->no_harvest[$key] ?? null,
+                        "kerosakan_id" => $request->kerosakan_id[$key] ?? null,
+                        "berat_tandan" => $request->berat_tandan[$key] ?? null,
+                        "catatan" => $request->catatan[$key] ?? null,
+                        "pengesah_id" => $request->pengesah_id[$key] ?? null,
+                        "catatan_pengesah" => $request->catatan_pengesah[$key] ?? null,
+                        "jumlah_tandan" => $request->jumlah_tandan[$key] ?? null,
+                        "status" => $request->status[$key] ?? null,
+                        "number_of_postponed_field" => $request->number_of_postponed_field[$key] ?? null,
+                        "tambah_hari" => $request->tambah_hari[$key] ?? null,
+                        "status_bunga" => $request->status_bunga[$key] ?? null,
+                        "status_bunga" => $request->status_bunga[$key] ?? null,
+                    ]);
                 }
-
-                $urlnew = $value->store(
-                    'harvest', 'public'
-                );
-
-                $harvest[$key]->update([
-                    'url_gambar' => $urlnew,
-                ]);
-
+    
             }
-
+    
+            if ($request->hasFile('url_gambar')) {
+                foreach ($request->file('url_gambar') as $key => $value) {
+    
+                    $image_path = $harvest[$key]->url_gambar;
+                    if (Storage::exists("public/" . $image_path)) {
+                        Storage::delete("public/" . $image_path);
+                    }
+    
+                    $urlnew = $value->store(
+                        'harvest', 'public'
+                    );
+    
+                    $harvest[$key]->update([
+                        'url_gambar' => $urlnew,
+                    ]);
+    
+                }
+    
+            }
         }
-
         return [
-            "harvest" => $harvest,
+            "harvest" => $harvest ?? null,
             "pokoks" => Pokok::all(),
             "tandans" => Tandan::all(),
             "korosakans" => Kerosakan::all(),
